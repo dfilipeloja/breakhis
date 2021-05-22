f8df82009e5fd2d7796abdbd43e47711ace901f7
dfilipeloja <dfilipeloja@gmail.com>
2021-05-22 09:39:56 +0100

    add train file and project's requirements

--- train.py
+++ train.py
@@ -1,1 +1,27 @@
 from keras.preprocessing.image import ImageDataGenerator
+from model import sequential
+
+classifier = sequential()
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('train',
+                                                 target_size = (350, 230),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('test',
+                                            target_size = (350, 230),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 250,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 1200)

b4706b7adc6c6d2929b006779c166f752c4f75e2
dfilipeloja <dfilipeloja@gmail.com>
2021-06-22 19:20:05 +0100

    predict

--- predict.py
+++ predict.py
@@ -19,7 +19,7 @@ model.compile(loss='binary_crossentropy',
                   optimizer = 'adam',
                   metrics=['accuracy'])
 
-filename = './validation/malignant/SOB_M_DC-14-3909-200-018.png'
+filename = './validation/benign/SOB_B_A-14-29960CD-200-007.png'
 
 img = image.load_img(
         filename,

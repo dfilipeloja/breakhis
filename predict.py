96f9e2851082407cc781fd3521a6dc2d8813d7d7
dfilipeloja <dfilipeloja@gmail.com>
2021-06-22 19:21:29 +0100

    predict

--- predict.py
+++ predict.py
@@ -4,15 +4,6 @@ from keras_preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix
 from keras.preprocessing import image
 
-test_datagen = ImageDataGenerator(rescale=1.0 / 255)
-test_dir = './validation'
-test_generator = test_datagen.flow_from_directory(
-        test_dir,
-        target_size=(400, 400),
-        batch_size=30,
-        class_mode='binary'
-    )
-
 model = load_model('./models/breakhis_vgg19_model.h5')
 
 model.compile(loss='binary_crossentropy',

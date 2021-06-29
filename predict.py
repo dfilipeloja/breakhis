0a77230ead7f5082864ba7ef8fdb25ac4a64dc87
dfilipeloja <dfilipeloja@gmail.com>
2021-06-29 20:09:21 +0100

    predict

--- predict.py
+++ predict.py
@@ -22,18 +22,20 @@ gen = test_datagen.flow_from_directory(
 images = []
 counter = 0
 
-for filename in gen.file:
-    img = image.load_img(
-            test_dir + '/' + filename,
-            target_size=(400, 400))
+print(gen.classes)
 
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = img / 255.0
-    print(os.path.basename(filename))
-    images.append(img)
-
-images = np.vstack(images[0:2])
+# for filename in gen.filenames:
+#     img = image.load_img(
+#             test_dir + '/' + filename,
+#             target_size=(400, 400))
+#
+#     img = image.img_to_array(img)
+#     img = np.expand_dims(img, axis=0)
+#     img = img / 255.0
+#     print(os.path.basename(filename))
+#     images.append(img)
+#
+# images = np.vstack(images[0:2])
 
 # Y_pred = model.predict(images, verbose=True)
 # Y_pred_classes = np.argmax(Y_pred,axis = 1)

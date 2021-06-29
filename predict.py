738b55abfcfe9539b9619f4180f2cefbd9a91b21
dfilipeloja <dfilipeloja@gmail.com>
2021-06-29 20:12:44 +0100

    predict

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import numpy as np
 from keras.models import load_model
 from keras.preprocessing import image
 from keras_preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
 
 model = load_model('./models/breakhis_vgg19_model.h5')
 
@@ -22,24 +23,25 @@ gen = test_datagen.flow_from_directory(
 images = []
 counter = 0
 
-print(gen.classes)
+y_true = gen.classes[0:2]
 
-# for filename in gen.filenames:
-#     img = image.load_img(
-#             test_dir + '/' + filename,
-#             target_size=(400, 400))
-#
-#     img = image.img_to_array(img)
-#     img = np.expand_dims(img, axis=0)
-#     img = img / 255.0
-#     print(os.path.basename(filename))
-#     images.append(img)
-#
-# images = np.vstack(images[0:2])
+for filename in gen.filenames:
+    img = image.load_img(
+            test_dir + '/' + filename,
+            target_size=(400, 400))
 
-# Y_pred = model.predict(images, verbose=True)
-# Y_pred_classes = np.argmax(Y_pred,axis = 1)
-# Y_true = np.argmax(y_test, axis = 1)
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+    print(os.path.basename(filename))
+    images.append(img)
+
+images = np.vstack(images[0:2])
+
+Y_pred = model.predict(images, verbose=True)
+Y_pred = Y_pred >= 0.3
+
+print(confusion_matrix(y_true, Y_pred))
 #
 # print(classes)
 #

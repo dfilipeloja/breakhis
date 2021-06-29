f30821f34bbad4138f6852a4c6d5b1e2578f0723
dfilipeloja <dfilipeloja@gmail.com>
2021-06-29 19:20:21 +0100

    predict

--- predict.py
+++ predict.py
@@ -1,8 +1,10 @@
+import os
+
 import numpy as np
 from keras.models import load_model
-from keras_preprocessing.image import ImageDataGenerator
-from sklearn.metrics import confusion_matrix
 from keras.preprocessing import image
+
+IMG_W, IMG_H = 400
 
 model = load_model('./models/breakhis_vgg19_model.h5')
 
@@ -10,22 +12,24 @@ model.compile(loss='binary_crossentropy',
                   optimizer = 'adam',
                   metrics=['accuracy'])
 
-filename = './validation/malignant/SOB_M_DC-14-3909-200-018.png'
+#filename = './validation/malignant/SOB_M_DC-14-3909-200-018.png'
+test_folder = './test'
 
-img = image.load_img(
-        filename,
-        target_size=(400, 400))
+images = []
 
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = x / 255.0
+for img in os.listdir(test_folder):
+    img = os.path.join(test_folder, img)
+    img = image.load_img(img, target_size=(IMG_W, IMG_H))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+    images.append(img)
 
-print(type(x))
-print(x.shape)
+print(images)
 
-pred = model.predict(x)
-
-if pred >= 0.5:
-    print(pred, f'{filename} is malignant')
-else:
-    print(pred, f'{filename} is benign')
+# pred = model.predict(x)
+#
+# if pred >= 0.5:
+#     print(pred, f'{filename} is malignant')
+# else:
+#     print(pred, f'{filename} is benign')

2861a25e6421b13432c04e5a8d7890558a2dd478
dfilipeloja <dfilipeloja@gmail.com>
2021-06-29 19:21:42 +0100

    predict

--- predict.py
+++ predict.py
@@ -19,14 +19,23 @@ images = []
 
 for img in os.listdir(test_folder):
     img = os.path.join(test_folder, img)
-    img = image.load_img(img, target_size=(IMG_W, IMG_H))
+    img = image.load_img(img, target_size=(400, 400))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     img = img / 255.0
     images.append(img)
 
-print(images)
+# img = image.load_img(
+#         filename,
+#         target_size=(400, 400))
+#
+# x = image.img_to_array(img)
+# x = np.expand_dims(x, axis=0)
+# x = x / 255.0
 
+# print(type(x))
+# print(x.shape)
+#
 # pred = model.predict(x)
 #
 # if pred >= 0.5:
